0e9e15b09068d9c213f9706e2c53f9911f3dea89
gauthamzz <thabeatsz@gmail.cmo>
2018-10-08 04:56:11 +0530

    finish tests in mocha

---
+++ ethereum/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+    'help novel two squirrel cook multiply flock cluster broccoli fold damp lonely',
+    'https://rinkeby.infura.io/v3/a5e7b7436ed6477da10aa1f0eb2c4844'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
